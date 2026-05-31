0ecdb1648ba54ec6c4058fd8b204b90afa15b980
Baky <rakabdu91@gmail.com>
2026-05-31 19:24:34 +0200

    Ajout de marges

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,10 @@ const PROJECTS_DATA = [
 
 export default function Projects() {
   return (
-    <section id='projects'>
+    <section
+      id='projects'
+      className='min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8 py-20'
+    >
       <div className='max-w-7xl mx-auto w-full'>
         <SectionTitle title='Projets' />
         <div className='grid md:grid-cols-2 gap-8'>
