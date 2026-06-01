d885bcd2fd4657d3021e9c2662b4f0d155b14591
Baky <rakabdu91@gmail.com>
2026-06-01 19:13:50 +0200

    Création des données pour les expériences

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -1,12 +1,39 @@
 import SectionTitle from '../layout/SectionTitle'
 
+const experiences = [
+  {
+    title: "Automatisation et Développement d'Interfaces",
+    company: 'Stellantis',
+    period: 'Septembre 2025 - Présent',
+    current: true,
+    description: [
+      'Modernisation de pipelines de modélisation 3D',
+      "• Développement d'un plugin de standardisation de modèles 3D sur Autodesk VRED",
+      "• Écriture de scripts d'automatisation de génération de rendus photos et vidéos",
+      "• Création d'interfaces utilisateurs",
+      "• Conception et développement d'une architecture Web (API REST, Base de données, Interface Web)",
+    ],
+  },
+]
+
 export default function Experience() {
+  console.log(experiences)
   return (
     <section
       id='experience'
       className='flex items-center justify-center px-4 sm:px-6 lg:px-8 py-20 bg-muted/30'
     >
       <SectionTitle title='Expérience' />
+      {experiences.map((exp) => (
+        <div key={exp.title}>
+          <h3>{exp.title}</h3>
+          <p>{exp.company}</p>
+          <p>{exp.period}</p>
+          {exp.description.map((line, idx) => (
+            <p key={'line-' + idx}>{line}</p>
+          ))}
+        </div>
+      ))}
     </section>
   )
 }
