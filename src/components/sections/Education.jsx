71ee79fd006d57787b0f0b3b616c994eb4f3ca39
Baky <rakabdu91@gmail.com>
2026-06-01 19:25:55 +0200

    Préparation données pour les formations

--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -1,6 +1,30 @@
 import SectionTitle from '../layout/SectionTitle'
 
+const formations = [
+  {
+    degree: '',
+    institution: 'ENSIIE, Évry-Courcouronnes',
+    period: '2026',
+    description: '',
+  },
+  {
+    degree:
+      "BUT Informatique (Parcours : Développement, Conception et Réalisation d'applications)",
+    institution: "IUT d'Orsay, Université Paris-Saclay",
+    period: '2024 - 2026',
+    description: '',
+  },
+  {
+    degree: 'Baccalauréat général (Mathématiques, Physiques-Chimie',
+    institution: 'Lycée Jacques Prévert, Longjumeau',
+    period: '2024',
+    description: '',
+  },
+]
+
 export default function Education() {
+  console.log(formations)
+
   return (
     <section
       id='education'
